1bee827e0b889a537b7df3fe1d084f9f43f61324
Letícia Felix <lehcfelixf@gmail.com>
2026-03-31 20:56:04 -0300

    exercício 2

---
+++ média entre cinco números.py
@@ -0,0 +1,7 @@
+# b) média aritimética de cinco números
+n1b = float(input("Digite o primeiro número: "))
+n2b = float(input("Digite o segundo número: "))
+n3b = float(input("Digite o terceiro número: "))
+n4b = float(input("Digite o quarto número: "))
+n5b = float(input("Digite o quinto número: "))
+media = (n1b + n2b + n3b + n4b + n5b) / 5
