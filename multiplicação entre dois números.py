6936382da01988af1910988df765118170f80cff
Letícia Felix <lehcfelixf@gmail.com>
2026-03-31 20:53:18 -0300

    exercício 1

---
+++ multiplicação entre dois números.py
@@ -0,0 +1,6 @@
+# a) multiplicação entre dois números
+n1a = float(input("Digite o primeiro número: "))
+n2a = float(input("Digite o segundo número: "))
+result_a = n1a * n2a
+
+print("O resultado da multiplicação é: ",result_a)
