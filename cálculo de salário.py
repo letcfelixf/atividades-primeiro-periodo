809cfb16bcb00607df65511443b9699addf7135e
Letícia Felix <lehcfelixf@gmail.com>
2026-03-31 21:41:03 -0300

    exercício 7

---
+++ cálculo de salário.py
@@ -0,0 +1,6 @@
+# g) cálculo de salário de acordo com horas trabalhadas
+horas = float(input("Digite aqui o total de horas trabalhadas: "))
+valor_hora = float(input("Digite aqui o valor por hora trabalhada: "))
+salario = horas * valor_hora
+
+print("O salário total é: ", salario)
