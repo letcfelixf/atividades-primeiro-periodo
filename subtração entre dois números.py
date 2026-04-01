2e6cba2305b086349fe859b91b9c15ac71359e40
Letícia Felix <lehcfelixf@gmail.com>
2026-03-31 21:18:56 -0300

    exercício 4

---
+++ subtração entre dois números.py
@@ -0,0 +1,6 @@
+# d) subtração entre dois números
+n1d = float(input("Digite o primeiro número: "))
+n2d = float(input("Digite o segundo número: "))
+result_d = n1d - n2d
+
+print("O resultado da subtração é: ",result_d)
