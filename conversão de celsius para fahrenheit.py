e9748ab80f330cc1aee8d62bac27565b833be830
Letícia Felix <lehcfelixf@gmail.com>
2026-03-31 21:39:06 -0300

    exercício 6

---
+++ conversão de celsius para fahrenheit.py
@@ -0,0 +1,5 @@
+# f) conversão de celsius para fahrenheit
+celsius = float(input("Digite aqui a temperatura em celsius: "))
+fahrenheit = (celsius * 9/5) + 32
+
+print("A temperatura",celsius, "°C, é igual",fahrenheit, "°F")
