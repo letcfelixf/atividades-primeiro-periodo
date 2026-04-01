a0ad19aa34f21f9828575bf4f648efc6222aed02
Letícia Felix <lehcfelixf@gmail.com>
2026-03-31 21:01:38 -0300

    exercício 3

---
+++ adição de 8% de imposto.py
@@ -0,0 +1,6 @@
+# c) adição de 8% de imposto no valor do produto
+valor_i = float(input("Digite o valor inicial do produto: "))
+imposto = 1.08
+valor_f = valor_i * imposto
+
+print("O valor final do produto é: ",valor_f)
