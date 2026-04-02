63e91f00c597c5b0ee726876ace0e7d96067a294
Letícia Felix <lehcfelixf@gmail.com>
2026-04-02 15:55:35 -0300

    exercício 13

---
+++ qual o maior entre dois números.py
@@ -0,0 +1,9 @@
+n1e = float(input("digite o primeiro número: "))
+n2e = float(input("digite o segundo número: "))
+
+if (n1e == n2e):
+    print("eles são iguais")
+elif (n1e > n2e):
+    print("o maior é: ", n1e)
+else:
+    print("o maior é: ",n2e)
