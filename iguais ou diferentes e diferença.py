d804a83a989434688b40fe1b4f3c702d00299139
Letícia Felix <lehcfelixf@gmail.com>
2026-04-07 09:20:25 -0300

    Create iguais ou diferentes e diferença.py

---
+++ iguais ou diferentes e diferença.py
@@ -0,0 +1,7 @@
+n1o = int(input("digite o primeiro número: "))
+n2o = int(input("digite o segundo número: "))
+n3o = n1o - n2o
+if n1o==n2o:
+    print("são iguais")
+else:
+    print("são diferentes, e a diferença entre eles é:", n3o )
