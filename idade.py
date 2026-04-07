19dbb9a895c8833bf6d295e3bd497a2ab506b817
Letícia Felix <lehcfelixf@gmail.com>
2026-04-07 09:10:03 -0300

    Update idade.py

--- idade.py
+++ idade.py
@@ -1,5 +1,7 @@
 n1m = int(input("digite sua idade aqui: "))
 if n1m>18:
     print("maior de idade")
-else: 
-    print("menor de idade")
+elif 59>n1m>18: 
+    print("adulto")
+else:
+    print("idoso")
