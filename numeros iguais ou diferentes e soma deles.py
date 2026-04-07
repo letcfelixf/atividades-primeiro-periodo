ab549796035fcc9c9016807e2f8ec9e80304678c
Letícia Felix <lehcfelixf@gmail.com>
2026-04-06 23:21:29 -0300

    Create numeros iguais ou diferentes e soma deles.py

---
+++ numeros iguais ou diferentes e soma deles.py
@@ -0,0 +1,9 @@
+n1k = int(input("Digite o primeiro número aqui: "))
+n2k = int(input("Digite o segundo número aqui: "))
+n3k = n1k + n2k
+
+print("a soma entre eles é: ",n3k)
+if (n1k == n2k):
+    print("eles são iguais")
+else:
+    print("eles são diferentes")
